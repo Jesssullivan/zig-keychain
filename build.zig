5e2b146df804ad71b264c726ff411e5d166bca81
Jess Sullivan <jess@sulliwood.org>
2026-03-28 22:24:01 -0400

    Fix Linux build: link libsecret-1 and glib-2.0 for @cImport
    The keychain_linux.zig @cImport needs libsecret headers. Without
    linkSystemLibrary, Zig can't find libsecret/secret.h on Linux.

--- build.zig
+++ build.zig
@@ -4,6 +4,9 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const resolved_target = target.result;
+    const is_linux = resolved_target.os.tag == .linux;
+
     // Static library for C FFI
     const lib = b.addLibrary(.{
         .name = "zig-keychain",
@@ -14,6 +17,12 @@ pub fn build(b: *std.Build) void {
         }),
         .linkage = .static,
     });
+
+    // Link libsecret on Linux (needed for @cImport of libsecret/secret.h)
+    if (is_linux) {
+        lib.root_module.linkSystemLibrary("libsecret-1", .{});
+        lib.root_module.linkSystemLibrary("glib-2.0", .{});
+    }
 
     b.installArtifact(lib);
 
@@ -27,5 +36,11 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
+
+    if (is_linux) {
+        t.root_module.linkSystemLibrary("libsecret-1", .{});
+        t.root_module.linkSystemLibrary("glib-2.0", .{});
+    }
+
     test_step.dependOn(&b.addRunArtifact(t).step);
 }
